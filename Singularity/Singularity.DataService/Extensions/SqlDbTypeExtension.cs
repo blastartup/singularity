183ffe05747b88ce6872a8e6413693f9a40bfeb9
Dean Goddard <dean@funkeysolutions.com.au>
2016-05-12 22:00:06 +1000

    Add new DbType, SqlDbType converter.

---
+++ Singularity/Singularity.DataService/Extensions/SqlDbTypeExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Singularity.DataService.ReferenceType;
+
+namespace Singularity.DataService.Extensions
+{
+	public static class SqlDbTypeExtension
+	{
+		public static Type ToNetType(this SqlDbType sqlDbType)
+		{
+			return TypeConverter.ToNetType(sqlDbType);
+		}
+
+		public static DbType ToDbType(this SqlDbType sqlDbType)
+		{
+			return TypeConverter.ToDbType(sqlDbType);
+		}
+	}
+}
---
+++ Singularity/Singularity.DataService/Extensions/TypeExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Singularity.DataService.ReferenceType;
+
+namespace Singularity.DataService.Extensions
+{
+	public static class TypeExtension
+	{
+		public static DbType ToNetType(this Type type)
+		{
+			return TypeConverter.ToDbType(type);
+		}
+
+		public static SqlDbType ToSqlDbType(Type type)
+		{
+			return TypeConverter.ToSqlDbType(type);
+		}
+	}
+}
