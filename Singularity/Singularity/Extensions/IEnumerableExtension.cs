d297440f4aaf29bdcb80bc83c0f0ee658596765b
Dean Goddard <dean@funkeysolutions.com.au>
2017-04-24 21:53:46 +1000

    Added Linq Join extension methods.

--- Singularity/Singularity/Extensions/IEnumerableExtension.cs
+++ Singularity/Singularity/Extensions/IEnumerableExtension.cs
@@ -129,7 +129,7 @@ namespace Singularity
 			return from item in items
 					 join otherItem in otherItems on getKey(item) equals getKey(otherItem) into tempItems
 					 from temp in tempItems.DefaultIfEmpty()
-						where ReferenceEquals(null, temp) || temp.Equals(default(T))
+					 where ReferenceEquals(null, temp) || temp.Equals(default(T))
 					 select item;
 
 		}
@@ -530,6 +530,88 @@ namespace Singularity
 		{
 			var methodCallExpression = (MethodCallExpression)method.Body;
 			return methodCallExpression.Method;
+		}
+
+		public static IEnumerable<TResult> LeftJoin<TSource, TInner, TKey, TResult>(this IEnumerable<TSource> source,
+																													IEnumerable<TInner> inner,
+																													Func<TSource, TKey> pk,
+																													Func<TInner, TKey> fk,
+																													Func<TSource, TInner, TResult> result)
+		{
+			return from s in source
+						 join i in inner
+						 on pk(s) equals fk(i) into joinData
+						 from left in joinData.DefaultIfEmpty()
+						 select result(s, left);
+		}
+
+
+		public static IEnumerable<TResult> RightJoin<TSource, TInner, TKey, TResult>(this IEnumerable<TSource> source,
+																													IEnumerable<TInner> inner,
+																													Func<TSource, TKey> pk,
+																													Func<TInner, TKey> fk,
+																													Func<TSource, TInner, TResult> result)
+		{
+			return from i in inner
+						 join s in source
+						 on fk(i) equals pk(s) into joinData
+						 from right in joinData.DefaultIfEmpty()
+						 select result(right, i);
+		}
+
+
+		public static IEnumerable<TResult> FullOuterJoinJoin<TSource, TInner, TKey, TResult>(this IEnumerable<TSource> source,
+																													IEnumerable<TInner> inner,
+																													Func<TSource, TKey> pk,
+																													Func<TInner, TKey> fk,
+																													Func<TSource, TInner, TResult> result)
+		{
+			var left = source.LeftJoin(inner, pk, fk, result).ToList();
+			var right = source.RightJoin(inner, pk, fk, result).ToList();
+
+			return left.Union(right);
+		}
+
+
+		public static IEnumerable<TResult> LeftExcludingJoin<TSource, TInner, TKey, TResult>(this IEnumerable<TSource> source,
+																													IEnumerable<TInner> inner,
+																													Func<TSource, TKey> pk,
+																													Func<TInner, TKey> fk,
+																													Func<TSource, TInner, TResult> result)
+		{
+			return from s in source
+						 join i in inner
+						 on pk(s) equals fk(i) into joinData
+						 from left in joinData.DefaultIfEmpty()
+						 where left == null
+						 select result(s, left);
+		}
+
+		public static IEnumerable<TResult> RightExcludingJoin<TSource, TInner, TKey, TResult>(this IEnumerable<TSource> source,
+																										 IEnumerable<TInner> inner,
+																										 Func<TSource, TKey> pk,
+																										 Func<TInner, TKey> fk,
+																										 Func<TSource, TInner, TResult> result)
+		{
+			return from i in inner
+						 join s in source
+						 on fk(i) equals pk(s) into joinData
+						 from right in joinData.DefaultIfEmpty()
+						 where right == null
+						 select result(right, i);
+		}
+
+
+		public static IEnumerable<TResult> FulltExcludingJoin<TSource, TInner, TKey, TResult>(this IEnumerable<TSource> source,
+																											 IEnumerable<TInner> inner,
+																											 Func<TSource, TKey> pk,
+																											 Func<TInner, TKey> fk,
+																											 Func<TSource, TInner, TResult> result)
+		{
+			var left = source.LeftExcludingJoin(inner, pk, fk, result).ToList();
+			var right = source.RightExcludingJoin(inner, pk, fk, result).ToList();
+
+			return left.Union(right);
 		}
 	}
 }
