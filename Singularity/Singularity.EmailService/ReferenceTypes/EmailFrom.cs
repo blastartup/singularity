ba9a59001711d69af3732f81896e885e0591173d
Dean Goddard <dean@funkeysolutions.com.au>
2017-05-17 22:59:54 +1000

    Add HashCode to EmailFrom.cs

--- Singularity/Singularity.EmailService/ReferenceTypes/EmailFrom.cs
+++ Singularity/Singularity.EmailService/ReferenceTypes/EmailFrom.cs
@@ -17,5 +17,16 @@ namespace Singularity.EmailService
 		{
 			return (obj is EmailFrom && ((EmailFrom)obj).Address == Address);
 		}
+
+		public override Int32 GetHashCode()
+		{
+			unchecked // Overflow is fine, just wrap
+			{
+				Int32 hash = (Int32)2166136261;
+				// Suitable nullity checks etc, of course :)
+				hash = hash * 16777619 ^ Address.GetHashCode();
+				return hash;
+			}
+		}
 	}
 }
