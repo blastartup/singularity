739c11e3b7b02e20f357ab5dcb5974c8ce0ee68a
Dean Goddard <dean@funkeysolutions.com.au>
2019-08-17 16:02:49 +1000

    Using .NET 4.6.1.  Add new SqlServer model.

---
+++ Singularity/Singularity.DataService/SqlFramework/Models/SqlServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singularity.DataService.SqlFramework.Models
+{
+	public class SqlServer
+	{
+		// SELECT @@servername
+		public String Name { get; set; }
+
+		// SELECT @@servicename
+		public String InstanceName { get; set; }
+
+		// ExecScalar("SELECT serverproperty('Edition')").ToString().StartsWith("Express")
+		public Boolean IsSqlServerExpress { get; set; }
+
+		// new Words(ServiceAccount, ValueLib.ForwardSlash.StringValue)[1]
+		public String ServiceName { get; set; }
+
+		// select ServiceName from sys.dm_server_services
+		public String DisplayName { get; set; }
+
+		public String Description { get; set; }
+
+		// select Service_Account from sys.dm_server_services
+		public String ServiceAccount { get; set; }
+	}
+}
--- Singularity/VersionInfo.cs
+++ Singularity/VersionInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyProduct("Singularity")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
-[assembly: AssemblyVersion("2.14.27")]
+[assembly: AssemblyVersion("2.14.28")]
